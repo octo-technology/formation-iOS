40d7ef47b20ad09f8c7da337c91e394f143b6ad2
julien pechon <julien.pechon@octo.com>
2022-03-12 22:15:44 +0100

    adding comments

--- formation-iOS/MyTableViewController.swift
+++ formation-iOS/MyTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class MyTableViewController: UIViewController {
     
+    // Custom array of data
     let characterArray = [
         Character(name: "Luke Skywalker", weight: "73", height: "172", origin: "tatooine", portrait: "Luke"),
         Character(name: "C-3PO", weight: "75", height: "171", origin: "tatooine", portrait: "C3PO"),
@@ -17,18 +18,17 @@ class MyTableViewController: UIViewController {
         Character(name: "Leia Organa", weight: "51", height: "150", origin: "alderaan", portrait: "Leia")
     ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+    // Changing viewController navigation title in viewWillAppear
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         self.navigationItem.title = "Star Wars"
     }
 }
 
-extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
+//MARK: UITableViewDataSource
+
+extension MyTableViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return characterArray.count
     }
@@ -40,17 +40,21 @@ extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
         
         return cell
     }
+}
+
+//MARK: UITableViewDelegate
+
+extension MyTableViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerCell = tableView.dequeueReusableCell(withIdentifier: "headerCell") as! HeaderTableViewCell
-        
+
         headerCell.titleLabel.text = "Personnages"
+        
         return headerCell
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
     }
-    
 }
-
